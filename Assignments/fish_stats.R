a2984f9a068252e17208e2448bc90672a06bb960
salbuque <salbuquerque@ucsb.edu>
2021-03-02 17:10:42 -0800

    update assignment 4

--- Assignments/fish_stats.R
+++ Assignments/fish_stats.R
@@ -1,7 +1,9 @@
-fish_stats = function(x, histogram) {
-  most_common = which.max(summary(x))
-  rarest_fish = which.min(summary(x))
-  fish_histogram <- ggplot(data = summary(x)) + geom_histogram()
-  total_fish  = sum(as.numeric(summary(x)))
+fish_stats = function(fish) {
+  
+  fish<- as.factor(fish[1])
+  most_common = which.max(summary(fish))
+  rarest_fish = which.min(summary(fish))
+  total_fish  = sum((summary(fish)))
+  
   return(list(most_common, rarest_fish, total_fish))
 }
